9c857cce2b8efac66928ea6eb391a143d89b838a
Fatih Turan <fatih@unicrow.com>
2025-10-29 22:04:27 +0300

    Update APY rates

--- custom-text-variables.js
+++ custom-text-variables.js
@@ -1,6 +1,9 @@
 // Custom text variables
 const acfData = {
-    "apyvalue": "3.14%",
+    "apyvalue": "2.93%",
+    "apytier1": "1.69%",
+    "apytier2": "2.39%",
+    "apytier3": "2.71%",
     "apymultiple": "60x",
     "nationalapy": "0.07%",
     "cashback_uppercase": "Unlimited 1%",
@@ -14,9 +17,6 @@ const acfData = {
     "fdic_short": "$3M",
     "fdic_long": "$3,000,000",
     "fdic_insurance_up": "$250,000",
-    "apytier1": "1.81%",
-    "apytier2": "2.55%",
-    "apytier3": "2.91%"
 };
 
 // Update the dynamic values
